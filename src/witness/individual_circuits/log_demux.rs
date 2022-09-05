28c596634c593b0df31767fb424b072a44f7d2ee
romanbrodetskiy <rb@matterlabs.dev>
2022-09-05 11:25:31 +0100

    more debug

--- src/witness/individual_circuits/log_demux.rs
+++ src/witness/individual_circuits/log_demux.rs
@@ -31,6 +31,10 @@ pub fn compute_logs_demux<
     artifacts: &mut FullBlockArtifacts<E>,
     round_function: &R,
 ) -> LogDemuxerCircuitInstanceWitness<E> {
+    for x in artifacts.demuxed_rollup_storage_queries {
+        println!(x);
+    }
+
     // parallelizable 
     
     // have to manually unroll, otherwise borrow checker will complain
