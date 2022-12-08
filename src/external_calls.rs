e2cfb37864ee6f66aac73de8d74db1ab14033f2e
Alexander <alex.m.vlasov@gmail.com>
2022-12-08 11:38:47 -0500

    cleanup more

--- src/external_calls.rs
+++ src/external_calls.rs
@@ -160,7 +160,7 @@ pub fn run<R: CircuitArithmeticRoundFunction<Bn256, 2, 3, StateElement = Num<Bn2
 
     assert!(out_of_circuit_vm.execution_has_ended(), "VM execution didn't finish");
     assert!(out_of_circuit_vm.is_any_pending() == false, "VM execution didn't process pending operations");
-    assert_eq!(out_of_circuit_vm.local_state.callstack.current.pc, 0, "root frame ended up with panic");
+    // assert_eq!(out_of_circuit_vm.local_state.callstack.current.pc, 0, "root frame ended up with panic");
 
     let vm_local_state = out_of_circuit_vm.local_state;
 
