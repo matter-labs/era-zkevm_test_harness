2438c288a0d19d8dae1a88706081ffa60d0dc220
Jules de Smit <julesdesmit@gmail.com>
2023-10-24 03:38:58 +0200

    Fix harness for current 1.4.1

--- src/witness/individual_circuits/storage_application.rs
+++ src/witness/individual_circuits/storage_application.rs
@@ -1,9 +1,9 @@
 use super::*;
 use crate::boojum::gadgets::keccak256::{self};
 use crate::boojum::sha3::digest::{FixedOutput, Update};
-use crate::boojum::sha3::Keccak256;
 use crate::witness::individual_circuits::keccak256_round_function::encode_kecca256_inner_state;
 use crate::witness::tree::*;
+use crate::zk_evm::sha3::Keccak256;
 use crate::zk_evm::zk_evm_abstractions::precompiles::keccak256::{
     transmute_state, KECCAK_PRECOMPILE_BUFFER_SIZE,
 };
