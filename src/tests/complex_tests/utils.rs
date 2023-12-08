4c5c2e0b1980310e744a14d1150d6064913ea4e7
Jules de Smit <julesdesmit@gmail.com>
2023-12-08 19:46:00 +0100

    fix compilation pipeline, clean up code

--- src/tests/complex_tests/utils.rs
+++ src/tests/complex_tests/utils.rs
@@ -22,6 +22,7 @@ const BASIC_TEST_COMMIT_HASH_LOCATION: &str =
     "src/tests/complex_tests/test_artifacts/basic_test_commit_hash";
 
 const SOLC_VERSION: &str = "v0.8.17";
+const ZKSOLC_VERSION: &str = "v1.3.18";
 const FILE_NAMES: [&str; 4] = [
     "ReentrancyGuard.sol",
     "Helper.sol",
@@ -51,24 +52,27 @@ pub fn read_basic_test_artifact() -> TestArtifact {
     };
 
     if !Path::new(BASIC_TEST_JSON_LOCATION).exists() || latest_hash != hash || no_hash {
-        let binary_name =
+        let solc_binary_name =
             get_solc_binary_name().expect("should be able to figure out a solc binary");
+        let zksolc_binary_name =
+            get_zksolc_binary_name().expect("should be able to figure out a zksolc binary");
 
-        // delay unwrapping so we always clean up after ourselves even in case of failure
-        let contract_json = compile_latest_test_contract(&binary_name);
-        delete_solc_binary(&binary_name);
-        delete_contracts_folder();
-        let contract_json = match contract_json {
-            Ok(c) => c,
-            Err(e) => panic!("{:?}", e),
-        };
-
-        // TODO: we need to also fetch precompiles and correctly put everything together in the
-        // JSON, raw contract data won't do
-        fs::write(BASIC_TEST_JSON_LOCATION, contract_json)
-            .expect("should be able to write contract json");
-        fs::write(BASIC_TEST_COMMIT_HASH_LOCATION, latest_hash)
-            .expect("should be able to write new commit hash");
+        match compile_latest_test_contract(&solc_binary_name, &zksolc_binary_name) {
+            Ok(contract_json) => {
+                // TODO: we need to also fetch precompiles and correctly put everything together in the
+                // JSON, raw contract data won't do
+                fs::write(BASIC_TEST_JSON_LOCATION, contract_json)
+                    .expect("should be able to write contract json");
+                fs::write(BASIC_TEST_COMMIT_HASH_LOCATION, latest_hash)
+                    .expect("should be able to write new commit hash");
+            }
+            Err(e) => {
+                delete_binary(&solc_binary_name);
+                delete_binary(&zksolc_binary_name);
+                delete_contracts_folder();
+                panic!("{:?}", e);
+            }
+        }
     }
 
     let basic_test_bytes = fs::read(BASIC_TEST_JSON_LOCATION).expect("failed reading file");
@@ -94,26 +98,58 @@ fn get_latest_commit_hash() -> String {
     body[8..47].to_owned()
 }
 
-fn compile_latest_test_contract(solc_binary_name: &str) -> Result<String, ArtifactError> {
-    download_solc_binary(solc_binary_name)?;
-    let mut full_path = std::env::current_dir().unwrap();
-    full_path.push(solc_binary_name);
-    // XXX windows?
-    fs::set_permissions(full_path.clone(), fs::Permissions::from_mode(0o777)).unwrap();
-    let mut solc = Compiler::new(full_path.to_str().unwrap().to_owned());
+fn download_compiler_binary(binary_name: &str) -> Result<PathBuf, ArtifactError> {
+    download_zksolc_binary(binary_name)?;
+    let mut full_path =
+        std::env::current_dir().map_err(|e| ArtifactError::DownloadFailed(e.to_string()))?;
+    full_path.push(binary_name);
+    match std::env::consts::OS {
+        "linux" | "macos" => {
+            fs::set_permissions(full_path.clone(), fs::Permissions::from_mode(0o777))
+                .map_err(|e| ArtifactError::DownloadFailed(e.to_string()))?
+        }
+        // XXX windows?
+        _ => {}
+    }
+
+    full_path
+}
+
+fn compile_latest_test_contract(
+    solc_binary_name: &str,
+    zksolc_binary_name: &str,
+) -> Result<Vec<u8>, ArtifactError> {
+    let _ = download_compiler_binary(zksolc_binary_name)?;
+    let solc_compiler_path = download_compiler_binary(solc_binary_name)?;
+    let mut solc = Compiler::new(
+        solc_compiler_path
+            .to_str()
+            .map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?
+            .to_owned(),
+    );
 
     download_contracts()?;
 
     let file_names = FILE_NAMES
         .iter()
         .map(|name| {
-            let mut path = std::env::current_dir().unwrap();
+            let mut path = std::env::current_dir()
+                .map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?;
             path.push("contracts");
             path.push(name);
             path
         })
         .collect::<Vec<PathBuf>>();
-    let mut output = compiler_solidity::standard_output(
+
+    // set zksolc as executable for compiler
+    let mut path =
+        std::env::current_dir().map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?;
+    path.push(zksolc_binary_name);
+    compiler_solidity::EXECUTABLE
+        .set(path)
+        .map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?;
+
+    Ok(compiler_solidity::standard_output(
         &file_names,
         vec![],
         &mut solc,
@@ -129,13 +165,8 @@ fn compile_latest_test_contract(solc_binary_name: &str) -> Result<String, Artifa
         None,
         None,
     )
-    .map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?;
-
-    println!("{:?}", output);
-
-    panic!();
-    // should return bytecode here
-    Ok("".to_owned())
+    .map_err(|e| ArtifactError::CompilationFailed(e.to_string()))?
+    .bytecode)
 }
 
 fn download_contracts() -> Result<(), ArtifactError> {
@@ -164,7 +195,15 @@ fn download_solc_binary(binary_name: &str) -> Result<(), ArtifactError> {
     download_to_disk(&url, binary_name)
 }
 
-fn delete_solc_binary(binary_name: &str) {
+fn download_zksolc_binary(binary_name: &str) -> Result<(), ArtifactError> {
+    let url = "https://github.com/matter-labs/zksolc-bin/releases/download/".to_owned()
+        + ZKSOLC_VERSION
+        + "/"
+        + binary_name;
+    download_to_disk(&url, binary_name)
+}
+
+fn delete_binary(binary_name: &str) {
     let _ = fs::remove_file(binary_name);
 }
 
@@ -194,28 +233,51 @@ fn download_to_disk(url: &str, write_location: &str) -> Result<(), ArtifactError
 }
 
 fn get_solc_binary_name() -> Result<String, ArtifactError> {
-    // as far as i know, no arm pre-compiles for solc
-    if std::env::consts::ARCH != "x86_64" {
-        return Err(ArtifactError::UnsupportedArch);
-    }
-
     match std::env::consts::OS {
-        "linux" => Ok("solc-static-linux".to_owned()),
-        "macos" => Ok("solc-macos".to_owned()),
+        "linux" => {
+            if std::env::consts::ARCH == "x86_64" {
+                Ok("solc-static-linux".to_owned())
+            } else {
+                Err(ArtifactError::UnsupportedArch)
+            }
+        }
+        "macos" => {
+            if std::env::consts::ARCH == "arm" {
+                Ok("solc-macos".to_owned())
+            } else {
+                Err(ArtifactError::UnsupportedArch)
+            }
+        }
         "windows" => Ok("solc-windows.exe".to_owned()),
         _ => Err(ArtifactError::UnsupportedOS),
     }
 }
 
-fn construct_sources_map() -> BTreeMap<String, Source> {
-    let mut sources = BTreeMap::new();
-    for file_name in FILE_NAMES {
-        sources.insert(
-            "contracts/".to_owned() + file_name,
-            Source::try_from(Path::new(&("./contracts/".to_owned() + file_name)))
-                .expect("should be able to grab source from contract"),
-        );
+fn get_zksolc_binary_name() -> Result<String, ArtifactError> {
+    match std::env::consts::OS {
+        "linux" => {
+            if std::env::consts::ARCH == "x86_64" {
+                Ok("zksolc-linux-amd64-musl-".to_owned() + ZKSOLC_VERSION)
+            } else {
+                // there is no precompiled arm build for linux solc so we can't proceed if we arent
+                // on an amd64 platform
+                Err(ArtifactError::UnsupportedArch)
+            }
+        }
+        "macos" => {
+            if std::env::consts::ARCH == "arm" {
+                Ok("zksolc-macosx-arm64-".to_owned() + ZKSOLC_VERSION)
+            } else {
+                Err(ArtifactError::UnsupportedArch)
+            }
+        }
+        "windows" => {
+            if std::env::consts::ARCH == "x86_64" {
+                Ok("zksolc-windows-amd64-gnu-".to_owned() + ZKSOLC_VERSION + ".exe")
+            } else {
+                Err(ArtifactError::UnsupportedArch)
+            }
+        }
+        _ => Err(ArtifactError::UnsupportedOS),
     }
-
-    sources
 }
