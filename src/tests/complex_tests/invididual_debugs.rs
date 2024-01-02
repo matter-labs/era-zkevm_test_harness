aa69786f5bd53421fff7402bcf84a87194de0144
Alexander <alex.m.vlasov@gmail.com>
2024-01-02 12:57:17 +0100

    Av merge v1.4.0 (#60)

--- src/tests/complex_tests/invididual_debugs.rs
+++ src/tests/complex_tests/invididual_debugs.rs
@@ -2,6 +2,8 @@ use super::*;
 
 #[cfg(test)]
 mod test {
+    use crate::proof_wrapper_utils::wrap_proof;
+
     use super::*;
     use circuit_definitions::encodings::recursion_request::RecursionQueueSimulator;
     use std::io::Read;
@@ -143,5 +145,37 @@ mod test {
         }
 
         test_recursive_circuit(circuit);
+    }
+
+    #[derive(serde::Serialize, serde::Deserialize)]
+    pub enum FriProofWrapper {
+        Base(ZkSyncBaseLayerProof),
+        Recursive(ZkSyncRecursionLayerProof),
+    }
+
+    #[test]
+    fn test_wrapper_layer() {
+        let proof_file_name = "proofs_fri_proof_33908687.bin";
+
+        let mut content = std::fs::File::open(proof_file_name).unwrap();
+        let mut buffer = vec![];
+        content.read_to_end(&mut buffer).unwrap();
+
+        let proof: FriProofWrapper = bincode::deserialize(&buffer).unwrap();
+
+        let vk_file_name = "scheduler_vk.json";
+
+        let mut content = std::fs::File::open(vk_file_name).unwrap();
+        let mut buffer = vec![];
+        content.read_to_end(&mut buffer).unwrap();
+
+        let vk: ZkSyncRecursionLayerVerificationKey = serde_json::from_slice(&buffer).unwrap();
+
+        let FriProofWrapper::Recursive(proof) = proof else {
+            panic!();
+        };
+
+        let config = WrapperConfig::new(1);
+        let _ = wrap_proof(proof, vk, config);
     }
 }
