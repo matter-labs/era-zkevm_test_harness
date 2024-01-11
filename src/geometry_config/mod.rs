724f7b52accd5646db9609219ee0f6b4774d6b32
perekopskiy <mikeson.dp@gmail.com>
2024-01-11 19:32:34 +0200

    Update geometry

--- src/geometry_config/mod.rs
+++ src/geometry_config/mod.rs
@@ -4,7 +4,7 @@ use crate::toolset::GeometryConfig;
 
 pub const fn get_geometry_config() -> GeometryConfig {
     GeometryConfig {
-        cycles_per_vm_snapshot: 5692,
+        cycles_per_vm_snapshot: 5585,
         cycles_code_decommitter_sorter: 117500,
         cycles_per_log_demuxer: 58750,
         cycles_per_storage_sorter: 46921,
@@ -12,9 +12,9 @@ pub const fn get_geometry_config() -> GeometryConfig {
         cycles_per_ram_permutation: 136714,
         cycles_per_code_decommitter: 2845,
         cycles_per_storage_application: 33,
-        cycles_per_keccak256_circuit: 672,
+        cycles_per_keccak256_circuit: 293,
         cycles_per_sha256_circuit: 2206,
-        cycles_per_ecrecover_circuit: 2,
+        cycles_per_ecrecover_circuit: 4,
         limit_for_l1_messages_pudata_hasher: 774,
     }
 }
