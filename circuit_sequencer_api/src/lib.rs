9e5d2516af131eaeef1d01cc65446a56c775f95d
mm-zk <mm@matterlabs.dev>
2024-03-07 13:00:37 +0100

    moved geometry config and heap commitments

--- circuit_sequencer_api/src/lib.rs
+++ circuit_sequencer_api/src/lib.rs
@@ -1,4 +1,13 @@
+#![feature(array_chunks)]
+
+pub mod geometry_config;
 pub mod proof;
 pub mod sort_storage_access;
+pub mod toolset;
+
+pub mod commitments;
+pub mod utils;
+
+pub use circuit_encodings::boojum;
 
 pub const INITIAL_MONOTONIC_CYCLE_COUNTER: u32 = 1024;
