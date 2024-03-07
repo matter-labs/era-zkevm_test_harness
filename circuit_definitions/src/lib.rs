4b59767bf1acb36234ee075f4e84e90df7ee8640
mm-zk <mm@matterlabs.dev>
2024-03-07 12:43:45 +0100

    Create circuits_encoding_crate

--- circuit_definitions/src/lib.rs
+++ circuit_definitions/src/lib.rs
@@ -58,7 +58,7 @@ pub fn eip4844_proof_config() -> ProofConfig {
 
 pub mod aux_definitions;
 pub mod circuit_definitions;
-pub mod encodings;
+pub use circuit_encodings as encodings;
 
 use crate::boojum::implementations::poseidon2::Poseidon2Goldilocks;
 pub use zk_evm::ethereum_types;
