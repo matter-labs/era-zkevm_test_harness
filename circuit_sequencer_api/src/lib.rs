16bdad3321f5fd502e084617a48f0f2608243147
mm-zk <mm@matterlabs.dev>
2024-03-07 14:25:16 +0100

    Export parameters for blobs

--- circuit_sequencer_api/src/lib.rs
+++ circuit_sequencer_api/src/lib.rs
@@ -11,3 +11,7 @@ pub mod utils;
 pub use circuit_encodings::boojum;
 
 pub const INITIAL_MONOTONIC_CYCLE_COUNTER: u32 = 1024;
+
+// Export parameters needed for 4844 blobs.
+pub use circuit_encodings::zkevm_circuits::eip_4844::input::ELEMENTS_PER_4844_BLOCK;
+pub use circuit_encodings::zkevm_circuits::scheduler::block_header::MAX_4844_BLOBS_PER_BLOCK;
