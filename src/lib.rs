033f9e3f21f1b03647eabb42803621157762cf2a
mm-zk <mm@matterlabs.dev>
2024-03-25 15:11:22 +0100

    remove duplicated allocator_api

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@
 #![feature(iter_next_chunk)]
 #![feature(associated_type_defaults)]
 #![feature(bigint_helper_methods)]
-#![feature(allocator_api)]
 #![allow(unused_imports)]
 #![allow(clippy::drop_ref)]
 
