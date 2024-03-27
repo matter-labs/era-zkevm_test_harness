a0dfef097b83ca14c547db93a650939bdf9687ae
mm-zk <mm@matterlabs.dev>
2024-03-27 15:04:05 +0100

    sepck fmt

--- circuit_definitions/src/circuit_definitions/base_layer/secp256r1_verify.rs
+++ circuit_definitions/src/circuit_definitions/base_layer/secp256r1_verify.rs
@@ -12,9 +12,9 @@ pub struct Secp256r1VerifyFunctionInstanceSynthesisFunction {
     _marker: std::marker::PhantomData<(F, R)>,
 }
 
+use zkevm_circuits::secp256r1_verify::fixed_base_mul_table::*;
 use zkevm_circuits::secp256r1_verify::input::*;
 use zkevm_circuits::secp256r1_verify::secp256r1_verify_function_entry_point;
-use zkevm_circuits::secp256r1_verify::fixed_base_mul_table::*;
 
 impl CircuitBuilder<F> for Secp256r1VerifyFunctionInstanceSynthesisFunction
 where
