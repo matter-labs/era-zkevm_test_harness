8251b809f89034d08d2a7e763ef9a9c6e1152987
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-03 13:20:03 +0200

    fix bug

--- src/witness/advancing_range.rs
+++ src/witness/advancing_range.rs
@@ -25,6 +25,7 @@ impl<'a, T: GetCycles> AdvancingRange<'a, T> {
     pub fn get_range(&mut self, acceptable_range: Range<u32>) -> Range<usize> {
         assert!(acceptable_range.start >= self.previous_range.start);
         assert!(acceptable_range.end >= self.previous_range.end);
+        self.previous_range = acceptable_range.clone();
 
         for x in self.data[self.start..].iter() {
             if x.cycles() < acceptable_range.start {
@@ -67,5 +68,40 @@ impl<T> GetCycles for (u32, T) {
 impl<T, U> GetCycles for (u32, T, U) {
     fn cycles(&self) -> u32 {
         self.0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::AdvancingRange;
+
+    const EMPTY: &[u32] = &[];
+    const INDICES: &[u32] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    #[test]
+    fn correct_usage() {
+        let mut r = AdvancingRange::new(INDICES);
+        assert_eq!(r.get_slice(0..0), EMPTY);
+        assert_eq!(r.get_slice(1..5), &[1, 2, 3, 4]);
+        assert_eq!(r.get_slice(6..11), &[6, 7, 8, 9]);
+        assert_eq!(r.get_slice(7..11), &[7, 8, 9]);
+        assert_eq!(r.get_slice(10..100), EMPTY);
+        assert_eq!(r.get_range(10..100), 10..10);
+    }
+
+    #[test]
+    #[should_panic]
+    fn incorrect_usage() {
+        let mut r = AdvancingRange::new(INDICES);
+        r.get_slice(3..6);
+        r.get_slice(1..7);
+    }
+
+    #[test]
+    #[should_panic]
+    fn incorrect_usage2() {
+        let mut r = AdvancingRange::new(INDICES);
+        r.get_slice(3..6);
+        r.get_slice(3..5);
     }
 }
