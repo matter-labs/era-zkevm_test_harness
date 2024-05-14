ddf7d88fcf19bb851c2bb40217fc8cb83a31a185
mm <mm@matterlabs.dev>
2024-05-14 13:57:53 +0200

    adding new test

--- src/tests/simple_tests/far_calls.rs
+++ src/tests/simple_tests/far_calls.rs
@@ -371,3 +371,104 @@ fn test_far_call_pay_for_memory_growth() {
         50,
     );
 }
+
+#[test_log::test]
+fn test_far_call_with_decommit() {
+    // For far_call, the first register is holding the 'FarCallABI', which consists of:
+    // 64 bytes of 'extra data' - [forwarding_byte, shard_id, constructor_call, system_byte, 32 bytes ergs]
+    // 64 bytes empty.
+    // 128 bytes of 'fat pointer' - [length, start] [memory_page, offset]
+    //
+    // makes 36 bytes of calldata in aux heap and calls with it, and then returns with fat ptr forward
+    let asm = r#"
+        .text
+        .file	"Test_26"
+        .rodata.cst32
+        .p2align	5
+    CPI0_0:
+	    .cell 30272441630670900764332283662402067049651745785153368133042924362431065855
+        .cell 30272434434303437454318367229716471635614919446304865000139367529706422272
+    CPI0_1:
+	    .cell 65536
+        .text
+        .globl	__entry
+    __entry:
+    .main:
+        add 1, r0, r1
+        ; run with 8k gas.
+        shl.s 14, r1, r1
+        near_call r1, @limited, @catch_all
+        ret.ok r0
+    limited:
+        ; put data from CPI0_0 into 64 bytes on AUX heap (st.2) 
+        add 64, r0, r2
+        add @CPI0_0[0], r0, r3
+        st.2.inc r2, r3, r2
+        add @CPI0_0[1], r0, r3
+        st.2 r2, r3
+        ; create ABI for far_call
+        ; not system call
+        add 0, r0, r1
+        shl.s 24, r1, r1
+        ; use 2 for forwarding mode
+        add 2, r1, r1
+        shl.s 32, r1, r1
+        ; give 6k gas
+        add 6000, r1, r1
+        shl.s 96, r1, r1
+        ; fat ptr length
+        add 36, r1, r1
+        ; fat ptr offset
+        shl.s 32, r1, r1
+        add 64, r1, r1
+        shl.s 64, r1, r1
+        ; we are calling address 1<<16 where we deploy other_asm code
+        add @CPI0_1[0], r0, r2
+        far_call r1, r2, @catch_all
+        ret.ok r0
+    catch_all:
+        ret.panic r0
+    "#;
+
+    // this one reads some calldata, including partially beyond the bound,
+    // and completely beyond the bound, and returns
+    let other_asm_start = r#"
+        .text
+        .file	"Test_26"
+        .rodata.cst32
+        .p2align	5
+        .text
+        .globl	__entry
+    __entry:
+    .main:
+        sstore r1, r1
+        ret.ok r0
+    "#;
+
+    let noop = r#"
+        nop
+    "#;
+    let other_asm_end = r#"
+        ret.ok r0
+    "#;
+
+    let other_asm = other_asm_start.to_string() + &noop.repeat(10000) + &other_asm_end;
+
+    let entry_bytecode = Assembly::try_from(asm.to_owned())
+        .unwrap()
+        .compile_to_bytecode()
+        .unwrap();
+    use crate::ethereum_types::Address;
+    // Putting address in kernel space, so that we can send events and l1 msg.
+    let other_address = Address::from_low_u64_be(1u64 << 16);
+    let other_bytecode = Assembly::try_from(other_asm.to_owned())
+        .unwrap()
+        .compile_to_bytecode()
+        .unwrap();
+
+    run_and_try_create_witness_for_extended_state(
+        entry_bytecode,
+        vec![(other_address, other_bytecode)],
+        500,
+    );
+}
