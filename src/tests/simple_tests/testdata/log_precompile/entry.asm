827978a28b6ef3d69f1ee305ed89a696db8db7ae
mm <mm@matterlabs.dev>
2024-05-17 09:49:40 +0200

    review fixes

--- src/tests/simple_tests/testdata/log_precompile/entry.asm
+++ src/tests/simple_tests/testdata/log_precompile/entry.asm
@@ -8,8 +8,6 @@
     .main:
         add 10000, r0, r1
         add 1000, r0, r10
-        ; TODO: we should create a proper ABI here, for now, just passing random data.
-        add 2, r0, r2
         ; precompile call - address 0x1, AuxData (additional costs - 0 for now)
         log.precompile r2, r3, r4
 
@@ -18,8 +16,6 @@
         ret.panic r0
     inner:
         to_l1 r0, r1
-        ; TODO: we should create a proper ABI here, for now, just passing random data.
-        add 2, r0, r2
 
         ; Add extra 16k cost (more than this near call has)
         add 1, r0, r3
