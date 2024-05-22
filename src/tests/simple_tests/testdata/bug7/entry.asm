a7235671a1efda5a917e6fd6ae9933c607906406
mm <mm@matterlabs.dev>
2024-05-22 16:02:32 +0200

    final cleanup bug 7

--- src/tests/simple_tests/testdata/bug7/entry.asm
+++ src/tests/simple_tests/testdata/bug7/entry.asm
@@ -8,15 +8,7 @@ CPI0_0:
         .text
         .globl	__entry
     __entry:
-    .main:
-
-        ptr.add r1, r2, r1
-
-        add 211, r0, r5
-        add 64, r0, r6
-        st.1 r6, r5
-        st.2 r6, r5
-        
+    .main:        
         ; use 2 for forwarding mode
         add 2, r1, r1
         shl.s 32, r1, r1
@@ -25,8 +17,6 @@ CPI0_0:
         add 1, r0, r5
         shl.s 32, r5, r5
         sub.s 1, r5, r5
-
-
         add r5, r1, r1
 
         shl.s 96, r1, r1
@@ -36,27 +26,10 @@ CPI0_0:
         ; fat ptr offset
         add 64, r1, r1
         shl.s 64, r1, r1
-        context.ergs_left r9
-
-        add 3, r0, r7
-        context.set_context_u128 r7
-
-
-        add @CPI0_0[0], r0, r5
-        context.ergs_left r9
-        ; extra cost
-        add 2000, r0, r2
-        log.decommit r5, r2, r3
 
         add @CPI0_0[0], r0, r2
-
         far_call r1, r2, @do_panic
 
-    
-        add 120, r0, r2
-        log.sread r2, r0, r5
-        add 25, r0, r10
-        log.event.first r10, r5, r10
 
         ret.ok r0
     do_panic:
